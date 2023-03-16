46f0d606231034b78c8deae42cff0cf74dbb2272
SamKinnon <garysamjones@gmail.com>
2023-03-16 12:15:19 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/* 
+ * Program to display Programming is like building a multilingual puzzle
+ */
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return(0);
+}
