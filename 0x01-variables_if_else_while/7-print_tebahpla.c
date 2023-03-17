3b418502463ffca04afc2b5970a1c1def80b0859
SamKinnon <garysamjones@gmail.com>
2023-03-17 14:41:38 -0700

    committed

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- * main -main function
+ * main - main function
  * Return: always 0
  */
 int main(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i < 10; i++)
-		putchar (i + '0');
+	for (i = 'z'; i >= 'a'; i--)
+		putchar(i);
 	putchar('\n');
 	return (0);
 }
