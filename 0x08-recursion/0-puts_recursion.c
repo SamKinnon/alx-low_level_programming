b2c0de929a5c4944d69c0461d164e16f2d05ab8d
root <root@DESKTOP-B9C1KJF.localdomain>
2023-04-05 17:32:35 +0200

    Puts with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function
+ * Return: always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	_putchar('\n');
+}
