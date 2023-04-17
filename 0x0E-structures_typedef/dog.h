6b8b7081347dd58f78c0a2bd6c29c731c10f785e
root <root@DESKTOP-B9C1KJF.localdomain>
2023-04-17 21:05:22 +0200

    Define new type dog_t with the new name

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - a struct to hold the dog infos
- * @name: the first member
- * @age: the second member
- * @owner: the third member
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
- * Descrption: descrption
+ * Description: Longer description
  */
 struct dog
 {
@@ -15,10 +15,15 @@ struct dog
 	char *owner;
 };
 /**
- * dog_t - struct dog using typedef
+ * dog_t - typedef for struct dog
  */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
-typedef struct dog dog_t;
 #endif
