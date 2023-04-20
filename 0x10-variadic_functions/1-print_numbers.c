626dc4314b3ea9b607c8473b7871bfcf3aae8ec3
root <root@DESKTOP-B9C1KJF.localdomain>
2023-04-20 18:10:39 +0200

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,19 +3,22 @@
 #include <stdio.h>
 
 /**
- * print_numbers
+ * print_numbers - function
+ * @separator:
+ * @...: ellipsis
+ * @n: argument
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list num;
 	unsigned int i;
 
-	va_start(num,n);
+	va_start(num, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(num,int));
+		printf("%d", va_arg(num, int));
 
-		if(i != (n-1) && separator != NULL)
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
