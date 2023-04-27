a64989b681b1c3ed6634d10feaaccd5c261b5db4
Sam <garysamjones@gmail.com>
2023-04-27 22:06:20 +0200

    List Length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * list_len - function
+ * @h: pointer
+ * Return: number of elements
+ *
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
 #ifndef LISTS_H
 #define LISTS_H
+#include <stddef.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -8,12 +9,15 @@
  *
  * Description: singly linked list node structure
  */
+
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
 size_t print_list(const list_t *h);
 
+size_t list_len(const list_t *h);
 #endif
