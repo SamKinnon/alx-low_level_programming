1841a38185689c9f003a7257d1939648f14b5bf2
Sam <garysamjones@gmail.com>
2023-05-09 01:19:12 +0200

    Header file and Read text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,17 +1,20 @@
 #include "main.h"
 #include <stdlib.h>
 /**
+ * read_textfile - function to return read text
+ * @filename: file name
+ * @letters: letters
+ * Return: j and 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t i,j;
+	ssize_t i, j;
 	ssize_t *buffer;
 	ssize_t fd;
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
-	
 	if (fd == -1)
 		return (0);
 
