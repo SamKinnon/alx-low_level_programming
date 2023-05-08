8420852b4237d417ba0e3bd7d04245590ade9b3a
Sam <garysamjones@gmail.com>
2023-05-09 01:14:35 +0200

    Header file and Read text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t i,j;
+	ssize_t *buffer;
+	ssize_t fd;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	i = read(fd, buffer, letters);
+	j = write(STDOUT_FILENO, buffer, i);
+	free(buffer);
+	close(fd);
+	return (j);
+}
